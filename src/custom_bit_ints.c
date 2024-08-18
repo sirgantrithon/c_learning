d7db996c39b19cd6a43b206676cb5a1d017a2d11
Ian Andrews <sirgantrithon@gmail.com>
2024-08-18 12:00:50 -0400

    Added a bit more whitespace to custom_bit_ints

--- src/custom_bit_ints.c
+++ src/custom_bit_ints.c
@@ -4,12 +4,15 @@
 int main(void) {
 
 #if defined(__clang__)
+
   // A 65 bit integer (1 bit for sign and 64 for data). Takes up 8 bytes + 1 bit
   // but has an alignment of 16 bytes.
   _BitInt(65) thang = 2;
   printf("Size of _BitInt(65): %lu", sizeof(thang));
+
 #else
   puts("This program isn't useful unless compiled with clang");
 #endif
+
   return EXIT_SUCCESS;
 }
